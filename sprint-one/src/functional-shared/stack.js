4a590be7ad620fce1d3d03bcaf316ae91a0e1f32
selfresonator <selfresonator@gmail.com>
2016-03-16 16:58:07 -0700

    Functionally shared stack complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.count = 0;
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  while(this.count > 0) {
+    var deletedVal = this.storage[this.count-1];
+    delete this.storage[this.count];
+    this.count--;
+    return deletedVal;
+  }
+};
+
+stackMethods.size = function() {
+  return this.count;
+};
