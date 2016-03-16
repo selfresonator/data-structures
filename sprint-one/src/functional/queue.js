85cb014c6fe356df855ca8279ab27737df232f7a
selfresonator <selfresonator@gmail.com>
2016-03-16 12:19:18 -0700

    Finished queues in functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+  var storageLength = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[count] = value;
+    count++;
+    storageLength++;
   };
 
   someInstance.dequeue = function(){
+    if (count > 0) {
+      count--;
+    }
   };
 
   someInstance.size = function(){
+  return count;
   };
 
   return someInstance;
