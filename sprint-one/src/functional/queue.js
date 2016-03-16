090b2126998640e07de27d2c30f72da354f833b6
selfresonator <selfresonator@gmail.com>
2016-03-16 15:39:47 -0700

    Finished stacks and queues

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,18 +4,32 @@ var Queue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
   var count = 0;
-  var storageLength = 1;
+  var indexLength = hiIndex-lowIndex;
+  var hiIndex = 0;
+  var lowIndex = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value){
-    storage[storageLength] = value;
+    if (storage[lowIndex]) {
+      storage[lowIndex+1] = storage[lowIndex];
+      storage[lowIndex] = value;
+    } else {
+      storage[lowIndex] = value;
+    }
     count++;
-    storageLength++;
+    hiIndex++;
   };
 
   someInstance.dequeue = function(){
     if (count > 0) {
+      var deletedVal = storage[hiIndex-1];
+      console.log(deletedVal)
+      console.log('hiIndex is:', hiIndex)
+      delete storage[hiIndex];
       count--;
+      hiIndex--;
+      return deletedVal;
     }
   };
 
