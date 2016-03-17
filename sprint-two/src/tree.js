ee247eb8434404ce17df14eec209593fcdaca4fd
selfresonator <selfresonator@gmail.com>
2016-03-17 12:31:15 -0700

    started part 2 data structures

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,20 @@
 var Tree = function(value){
   var newTree = {};
+  console.log("inside Tree",value);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  // storage holding nodes
+  newTree.node = {};
+  _.extend(newTree,treeMethods);
+  newTree.children = [];
+  console.log(newTree.children)
   return newTree;
 };
-
-
-  // your code here
-  newTree.children = null;  // fix me
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  console.log('inside tree.addchild', value)
+  this.children[0] = value
 };
 
 treeMethods.contains = function(target){
